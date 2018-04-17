fcf16ced398460b7bf808e50194447fe62cc1752
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-04-17 17:27:03 -0500

    volverFormaControlada

--- src/pages/pagina3/pagina3.ts
+++ src/pages/pagina3/pagina3.ts
@@ -11,9 +11,16 @@ export class Pagina3Page {
 
   juegos: any = {};//se crea este objeto para que los elementos enviados desde "pagina2.html" puedan guardarse para mostrarse o utilizarse en esta pagina
 
-  constructor(private NavParams:NavParams) {
+  constructor(private NavParams:NavParams, private NavCtrl:NavController) {
     this.juegos = this.NavParams.get("game");//la palabra "game" es la que se coloco en "this.navCtrl.push( Pagina3Page, {'game':juego})" en "pagina2.ts"
   }
 
+  irAtras(){
+    this.NavCtrl.pop();//se coloca en el constructor una variable que tenga como referencia el "NavController" y se utiliza la funsion "pop"
+  }
+
+  irRoot(){
+    this.NavCtrl.popToRoot();//se llama la funsion "popToRoot" con la variable asignada para el "NavController"
+  }
 
 }
