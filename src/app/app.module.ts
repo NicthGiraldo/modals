2a7968d974ff10ebb03d5d7658e117f9b51511dc
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-04-17 12:20:33 -0500

    creandoPaginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,27 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import {
+  PrincipalPage,
+  AjustesPage,
+  Ajustes2Page,
+  ModalPage,
+  Pagina2Page,
+  Pagina3Page,
+  TabsPage
+} from '../pages/index.paginas';
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PrincipalPage,
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina2Page,
+    Pagina3Page,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +36,19 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PrincipalPage,
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina2Page,
+    Pagina3Page,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/informacion.ts
@@ -0,0 +1,6 @@
+/*en el commit "creandoPaginas" se crearon todas las paginas por medio del generador de ionic con la siguiente
+linea de comando "ionic g page nombrePagina", se agregaron las paginas en "index.paginas.ts" con el metodo "export"
+para llamarlas de una mejor forma (mas organizada) en "app.modules.ts", ya que todos los componentes y paginas
+tienes que ser llamadas en este documento.
+se importaron todas las paginas de "index.paginas.ts" y luego se acomodaron en los campos "declarations"
+y "entryComponents", cuando es solo un componente (no una pagina) solo se coloca en "declarations"*/
