765f7cc7f48270ec4f7aa8c10bc17cede6543db4
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-04-17 15:11:51 -0500

    pasarPagina

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Pagina2Page } from "../index.paginas";
 
 @IonicPage()
 @Component({
@@ -8,12 +8,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'principal.html',
 })
 export class PrincipalPage {
-
+  pagina2:any = Pagina2Page;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PrincipalPage');
+  navegarPagina(){
+    this.navCtrl.push(Pagina2Page);
   }
 
 }
